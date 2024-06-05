8a6ef54eb39da12ba06aa70586e83331a9417b3a
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2024-06-05 22:52:38 +0300

    Update openshiftDeployment.groovy

--- vars/openshiftDeployment.groovy
+++ vars/openshiftDeployment.groovy
@@ -8,9 +8,9 @@ def call() {
     //   sh "oc login https://api.ocp-training.ivolve-test.com:6443 --username=${USERNAME} --password=${PASSWORD} "
     //}
 
-     withCredentials([string(credentialsId: 'saTocken', variable: 'OC_TOKEN')]) {
+     withCredentials([string(credentialsId: 'saTocken', variable: 'OpenShift_CREDENTIALS')]) {
     // Login to OpenShift using the service account token
-         sh "oc login https://api.ocp-training.ivolve-test.com:6443 --token=${OC_TOKEN} --insecure-skip-tls-verify "
+         sh "oc login https://api.ocp-training.ivolve-test.com:6443 --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify "
      }
     // Deploy the image on the project
     sh "oc apply -f deployment.yml"
