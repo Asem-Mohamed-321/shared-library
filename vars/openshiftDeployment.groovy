01bd20310e61113e81cf225da96fc1de2be5d302
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2024-06-05 21:42:17 +0300

    Create openshiftDeployment.groovy

---
+++ vars/openshiftDeployment.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+// ocCredentialsID 'openshift'
+def call() {
+
+    // login to oc Cluster
+    withCredentials([usernamePassword(credentialsId: "openshift", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "oc login https://api.ocp-training.ivolve-test.com:6443 --username=${USERNAME} --password=${PASSWORD} "
+    }
+
+    // Deploy the image on the project
+    
+}
