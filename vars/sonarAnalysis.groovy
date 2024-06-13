7f63e3cb5b1bc1026a261703ca5167b93ded62ed
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2024-06-13 08:04:26 +0300

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -5,6 +5,7 @@ def call() {
       withSonarQubeEnv(installationName: 'sonarqube-server') { 
       //sh './gradlew clean org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.0.2155:sonar'
        sh './gradlew sonarqube'
+      }
 }
 
 
