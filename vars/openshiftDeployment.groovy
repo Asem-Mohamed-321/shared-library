f2b6ae779e6f0a70759a09cf1a2e120a884e92de
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2024-06-13 08:53:10 +0300

    Update openshiftDeployment.groovy

--- vars/openshiftDeployment.groovy
+++ vars/openshiftDeployment.groovy
@@ -6,13 +6,13 @@ def call(String imageName) {
 
      withCredentials([string(credentialsId: 'jsa2', variable: 'OC_TOKEN')]) {
      // Login to OpenShift using the service account token
-        sh "oc login https://api.ocp-training.ivolve-test.com:6443 --token=${OC_TOKEN} --insecure-skip-tls-verify "
+        sh "oc login https://console-openshift-console.apps.ocp-training.ivolve-test.com/  --token=${OC_TOKEN} --insecure-skip-tls-verify "
     }
 
     // sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"  // change the image tag in the deployment file
      
     // Deploy the image on the project
-    sh "oc apply -f deployment.yml"
-    sh "oc apply -f service.yml"
+    sh "oc apply -f deployment.yml -n asemmohamed"
+    sh "oc apply -f service.yml -n asemmohamed"
     
 }
