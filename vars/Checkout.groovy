dd0cfb7d4d6ac44d1fde1ad41b37aed8fe3f408e
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2024-06-13 08:40:51 +0300

    Update Checkout.groovy

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -1,6 +1,6 @@
 #!usr/bin/env groovy
 def call(String branch) {
 
-                    git branch: '${branch}', url: 'https://github.com/Asem-Mohamed-321/MultiCloudDevOpsProject.'
+                    git branch: ${branch} , url: 'https://github.com/Asem-Mohamed-321/MultiCloudDevOpsProject.'
 
 }
