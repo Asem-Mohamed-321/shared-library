e93d7b246f46df7c3ca0e0f423f7abde4033e8dc
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2024-06-13 08:44:40 +0300

    Update openshiftDeployment.groovy

--- vars/openshiftDeployment.groovy
+++ vars/openshiftDeployment.groovy
@@ -9,7 +9,7 @@ def call(String imageName) {
         sh "oc login https://api.ocp-training.ivolve-test.com:6443 --token=${OC_TOKEN} --insecure-skip-tls-verify "
     }
 
-     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"  // change the image tag in the deployment file
+    // sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yml"  // change the image tag in the deployment file
      
     // Deploy the image on the project
     sh "oc apply -f deployment.yml"
